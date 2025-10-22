c0663b030fd5c513ba9e6707d075607e6f35d3d9
mdmoto <61302600+mdmoto@users.noreply.github.com>
2025-10-22 22:14:26 +0900

    Update API endpoints to AWS server 18.209.174.139

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -9,9 +9,9 @@ var BASE = {
     manager: "http://localhost:9999"
   },
   API_PROD: {
-    common: "http://localhost:9999",
-    buyer: "http://localhost:9999",
-    seller: "http://localhost:9999",
-    manager: "http://localhost:9999"
+    common: "http://18.209.174.139:8890",
+    buyer: "http://18.209.174.139:8888",
+    seller: "http://18.209.174.139:8889",
+    manager: "http://18.209.174.139:8887"
   },
 };
--- seller/public/config.js
+++ seller/public/config.js
@@ -9,19 +9,19 @@ var BASE = {
     manager: "http://localhost:9999",
   },
   API_PROD: {
-    common: "http://localhost:9999",
-    buyer: "http://localhost:9999",
-    seller: "http://localhost:9999",
-    manager: "http://localhost:9999",
+    common: "http://18.209.174.139:8890",
+    buyer: "http://18.209.174.139:8888",
+    seller: "http://18.209.174.139:8889",
+    manager: "http://18.209.174.139:8887",
   },
   /**
    * @description // 跳转买家端地址 pc端
    */
-  PC_URL: "https://pc-b2b2c.maollar.cn",
+  PC_URL: "https://pc-b2b2c.maollar.com",
   /**
    * @description  // 跳转买家端地址 wap端
    */
-  WAP_URL: "https://m-b2b2c.maollar.cn",
+  WAP_URL: "https://m-b2b2c.maollar.com",
   /**
    *  @description api请求基础路径前缀
    */
